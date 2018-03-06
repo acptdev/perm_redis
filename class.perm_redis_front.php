e3e4e7525d07e0f567712e01af45f4c0c02e9eed
GIVAILSON DE SOUZA NEVES <givailson@gmail.com>
2018-03-06 13:20:55 -0300

    Create class.perm_redis_front.php
    criação do front do projeto

---
+++ class.perm_redis_front.php
@@ -0,0 +1,24 @@
+<?php 
+  class PermRedisDAO extends WP_Widget{  
+    // Main constructor
+    public function __construct() {
+      /* ... */
+    }
+
+    // The widget form (for the backend )
+    public function form( $instance ) {	
+      /* ... */
+    }
+
+    // Update widget settings
+    public function update( $new_instance, $old_instance ) {
+      /* ... */
+    }
+
+    // Display the widget
+    public function widget( $args, $instance ) {
+      /* ... */
+    }
+  }
+
+?>
