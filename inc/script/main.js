a3eb99426f734f34e83b5a1cedeca7a40d510a4e
GIVAILSON DE SOUZA NEVES <givailson@gmail.com>
2018-03-14 09:39:51 -0300

    ajustes

--- inc/script/main.js
+++ inc/script/main.js
@@ -1,129 +1,145 @@
-function Main(){
+angular.module("PermRedisApp", ['ui.utils.masks'])
+    .controller("PermRedisAppController",function($scope, $http, $q){
 
-    var _self = this;
-    var _registros = [];
-    var _pessoaEditada = null;
-    var _cargos = [];
-    var _regioes = [];
+        $scope.loaded = true;
+        $scope.regioes = [];
+        $scope.cargos = [];
+        $scope.registros = [];
+        $scope.tipoRegistro = "0";
+        $scope.tipoAba = "";
+        $scope.lotacaoOrigem = 1;
+        $scope.lotacaoDestino = 1;
+        $scope.emailRecuperacao = "";
+        $scope.sendingEdicao = false;
+        $scope.especialidades = [];
+        $scope.sendingCadastro = false;
+        $scope.cadastro = {
+            tipo: "1",
+            orgaoOrigem: 1,
+            orgaoDestino: 1,
+            cargo: 1,
+        };
 
-    var _init = function(){        
-        _loadRegioes();
-    }   
-
-    var _loadRegioes = function () {        
-
-        httpRequest("/regioes/", "GET",
-            null,
-            (_sucesso, _res) => {
-                if (_sucesso) {
-                    _regioes = _res;
-                    for (var i = 0; i < _res.length; i++) {
-                        jQuery('.perm_redis_form #orgaoOrigem')
-                            .append(jQuery('<option>').attr('value', _res[i].Id).text(_res[i].Nome));
-
-                        jQuery('.perm_redis_form #orgaoDestino')
-                            .append(jQuery('<option>').attr('value', _res[i].Id).text(_res[i].Nome));
-                    }
-                    _loadCargos();
-                } else {
-                    showFeedback(_res)
-                }
-            }
-        );
-    }
-
-    var _loadCargos = function () {
-        httpRequest("/cargos", "GET",
-            null,
-            (_sucesso, _res) => {
-                if (_sucesso) {
-                    _cargos = _res;
-                    for (var i = 0; i < _cargos.length; i++) {
-                        jQuery('.perm_redis_form #cargo')
-                            .append(jQuery('<option>').attr('value', _cargos[i].Id).text(_cargos[i].Nome));
-                    }
-                    _loadRegistros();
-                } else {
-                    showFeedback(_res)
-                }
-            }
-        );        
-    }
-
-
-    var _loadRegistros = function () {
-        httpRequest("/registros-front", "GET",
-            null,
-            (_sucesso, _res) => {
-                if (_sucesso) {
-                    _registros = _res;
-                   _prepareList();
-                } else {
-                    showFeedback(_res)
-                }
-            }
-        )
-    }
-
-    /**
-     * preparo de listagem
-     */
-    var _prepareList = function()
-    {
-        for(var i = 0; i < _registros.length; i++)
+        var _init = function()
         {
-            jQuery('#perm_redis_main .anuncios-content').append(`
-                <div class="perm_redis_main-item-list tipo_${_registros[i].tipo}">
-                    <div class="perm_redis_main-item-list-header text-right">
-                        Cadastrada em: ${dateFormat(_registros[i].data)}
-                    </div>
-                    <div class="perm_redis_main-item-list-info">
-                        <p><strong>Servidor:</strong> ${_registros[i].nome}</p>
-                        <p><strong>Cargo:</strong> ${_cargo_by_id(_registros[i].cargo).Nome}</p>                        
-                        <p><strong>Lotação Origem:</strong> ${_regiao_by_id(_registros[i].orgaoOrigem).Nome}</p>
-                        <p><strong>Cidade Origem:</strong> ${_registros[i].origem}</p>
-                        <p><strong>Orgão destino:</strong> ${_regiao_by_id(_registros[i].orgaoDestino).Nome}</p>                        
-                        <p><strong>Cidade destino:</strong> ${_registros[i].destino}</p>                                                
-                    </div>
-                    <hr />
-                    <div class="perm_redis_main-item-list-content">
-                        <p><strong>Telefone:</strong> ${_registros[i].telefone}</p>
-                        <p><strong>E-mail:</strong> ${_registros[i].email}</p>
-                        <p><strong>Mensagem:</strong> ${_registros[i].mensagem}</p>
-                    </div>
-                </div>
-            `);
+            _loadAll();
         }
-    }
 
-
-    var _pessoa_by_id = function(_id){
-        for (var i = 0; i < _registros.length; i++){
-            if (_registros[i].id == _id) 
-                return _registros[i];
+        var _loadAll = function()
+        {
+            $q.all([
+                $http.get(`${window.BASE_URL}/regioes`),
+                $http.get(`${window.BASE_URL}/cargos`),
+                $http.get(`${window.BASE_URL}/registros-front`)
+            ]).then( datas => {
+                $scope.loaded = false;
+                $scope.regioes = datas[0].data;
+                $scope.cargos = datas[1].data;
+                $scope.registros = datas[2].data;
+                _loadEspecialidades(1);
+            })
         }
-    }
 
-    var _cargo_by_id = function (_id) {
-        for (var i = 0; i < _cargos.length; i++) {
-            if (_cargos[i].Id == _id)
-                return _cargos[i];
+        var _loadEspecialidades = function(_id)
+        {
+            $http.get(`${window.BASE_URL}/especialidades/${_id}`)
+                .then(
+                    res => { 
+                        $scope.especialidades = res.data;
+                        $scope.cadastro.especialidade = $scope.especialidades[0].Id;
+                    }
+                )
         }
-    }
 
-    var _regiao_by_id = function (_id) {
-        for (var i = 0; i < _regioes.length; i++) {
-            if (_regioes[i].Id == _id)
-                return _regioes[i];
+        $scope.trocarAba = function(_val)
+        {
+            $scope.tipoAba = $scope.tipoAba == _val ? '' : _val;
+            if($scope.tipoAba == ""){
+                $scope.lotacaoOrigem = 1;
+                $scope.lotacaoDestino = 1;
+                $scope.cadastro = {
+                    tipo: "1",
+                    orgaoOrigem: 1,
+                    orgaoDestino: 1,
+                    cargo: 1,
+                };
+            }
         }
-    }    
-    var _tipo_by_id = function(_id)
-    {
-        return ["Não informado","Permuta", "Reedistribuição"][_id];
-    }
 
-    _init();
-}
-jQuery( document ).ready( () => {
-    new Main();
-});
+        $scope.edicaoPerfil = function(event)
+        {
+            event.preventDefault();
+            $scope.sendingEdicao = true;
+            $http.post(`${window.BASE_URL}/registro-edicao`, { "email": $scope.emailRecuperacao})
+                .then( res => {
+                    $scope.emailRecuperacao = "";
+                    window.alert('Email enviado com sucesso!');
+                    $scope.sendingEdicao = false;
+                }).catch(error => { $scope.sendingEdicao = false })
+        }
+
+        $scope.cadastrar = function()
+        {
+            $scope.sendingCadastro = true;
+            $http.post(`${window.BASE_URL}/registro`, $scope.cadastro)
+                .then(
+                    res => {
+                        $scope.sendingCadastro = false;
+                        $scope.cadastro = {
+                            tipo: "1",
+                            orgaoOrigem: 1,
+                            orgaoDestino: 1,
+                            cargo: 1,
+                        };
+                        $scope.trocarAba('');
+                        window.alert("Cadastro realizado com sucesso");
+                    }
+                ).catch(
+                    error => {
+                        $scope.sendingCadastro = false;
+                        window.alert("Erro ao cadastrar registro");
+                    }
+                )
+        }
+
+        $scope.selecionarRegiaoOrigem = function()
+        {
+            $http.get(`${window.BASE_URL}/estados/${$scope.cadastro.orgaoOrigem}`)
+                .then(
+                    res => { 
+                        $scope.estadosOrigem = res.data;
+                        $scope.cidadesOrigem = [];
+                    }
+                )
+            
+        }
+        $scope.selecionarRegiaoDestino = function()
+        {
+            $http.get(`${window.BASE_URL}/estados/${$scope.cadastro.orgaoDestino}`)
+                .then(
+                    res => { 
+                        $scope.estadosDestino = res.data;
+                        $scope.cidadesDestino = [];
+                    }
+                )
+        }
+        $scope.mudarEstadoOrigem = function()
+        {
+            $http.get(`${window.BASE_URL}/cidades/${$scope.cadastro.estadoOrigem}`)
+                .then( res => {
+
+                    $scope.cidadesOrigem = res.data;
+                    $scope.cadastro.cidadeOrigem = $scope.cidadesOrigem[0].perm_redis_cidade_id;
+                });
+
+        }
+        $scope.mudarEstadoDestino = function () {            
+            $http.get(`${window.BASE_URL}/cidades/${$scope.cadastro.estadoDestino}`)
+                .then(res => {
+
+                    $scope.cidadesDestino = res.data;
+                    $scope.cadastro.cidadeDestino = $scope.cidadesDestino[0].perm_redis_cidade_id;
+                });
+        }
+        _init();
+    });
